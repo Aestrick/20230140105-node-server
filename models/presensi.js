65905747e12166dda88ad96e2c089155cc3cb04c
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-11-05 05:59:28 +0700

    feat(TUGAS): define User-Presensi database relationship

--- models/presensi.js
+++ models/presensi.js
@@ -5,12 +5,14 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Presensi extends Model {
     /**
-    * Helper method for defining associations.
-    * This method is not a part of Sequelize lifecycle.
-    * The `models/index` file will call this method automatically.
-    */
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      // define association here
+      // TUGAS: Definisikan relasi di sini
+      // Satu Presensi dimiliki oleh satu User
+      Presensi.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
     }
   }
   Presensi.init({
